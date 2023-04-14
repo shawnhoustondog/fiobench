da37afbca0a199195492fbf3e348f781e5d81caa
Shawn Houston <houston.shawn@gmail.com>
2023-04-14 14:12:42 -0500

    static testing 2

--- fio.sh
+++ fio.sh
@@ -8,10 +8,11 @@ SLEEP=1h
 # start up delay...
 #sleep 30s
 
-mkdir -p ${SCRATCH}/${NODE_NAME}/${HOSTNAME}
+#mkdir -p ${SCRATCH}/${NODE_NAME}/${HOSTNAME}
+mkdir ${HOSTNAME}
 touch ${OUTPUT}/fiobench.${HOSTNAME}
 
-fio $CONFIGS/fio.job --eta=never --directory=${SCRATCH}/${NODE_NAME}/${HOSTNAME} --output=${OUTPUT}/fiobench.${HOSTNAME}
+fio $CONFIGS/fio.job --eta=never --directory=${HOSTNAME} --output=${OUTPUT}/fiobench.${HOSTNAME}
 
 # sleep forever
 while true;do sleep 1h;done
